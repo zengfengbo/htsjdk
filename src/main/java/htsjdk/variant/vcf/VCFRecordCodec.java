3b7cd82429f25a7322167968012e767fdfdcd742
Jay Carey <jcarey@broadinstitute.org>
2015-02-09 11:11:37 -0500

    SBT build.

---
+++ src/main/java/htsjdk/variant/vcf/VCFRecordCodec.java
@@ -0,0 +1,58 @@
+package htsjdk.variant.vcf;
+
+import htsjdk.samtools.util.SortingCollection;
+import htsjdk.variant.variantcontext.VariantContext;
+
+import java.io.*;
+
+/**
+ * Writes VariantContext instances to an OutputStream without headers or metadata. For use
+ * with SortingCollection ONLY.
+ */
+public class VCFRecordCodec implements SortingCollection.Codec<VariantContext> {
+
+    final VCFCodec vcfDecoder = new VCFCodec();
+
+    final VCFEncoder vcfEncoder;
+
+    private PrintStream outputStream = null;
+
+    private BufferedReader inputReader = null;
+
+    public VCFRecordCodec(final VCFHeader header) {
+        this.vcfEncoder = new VCFEncoder(header, false, false);
+        // Explicitly set the version because it's not available in the header itself.
+        this.vcfDecoder.setVCFHeader(header, VCFHeaderVersion.VCF4_1);
+    }
+
+    @Override
+    public void setOutputStream(final OutputStream stream) {
+        this.outputStream = new PrintStream(stream);
+    }
+
+    @Override
+    public void setInputStream(final InputStream stream) {
+        this.inputReader = new BufferedReader(new InputStreamReader(stream));
+    }
+
+    @Override
+    public void encode(final VariantContext context) {
+        this.outputStream.println(this.vcfEncoder.encode(context));
+    }
+
+    @Override
+    public VariantContext decode() {
+        try {
+            final String line;
+            return ((line = inputReader.readLine()) != null) ? this.vcfDecoder.decode(line) : null;
+        } catch (final IOException ioe) {
+            throw new RuntimeException("Could not decode/read a VCF record for a sorting collection: " + ioe.getMessage(), ioe);
+        }
+    }
+
+    @Override
+    public VCFRecordCodec clone() {
+        return new VCFRecordCodec(this.vcfEncoder.getVCFHeader());
+    }
+}
+
---
+++ src/test/java/htsjdk/variant/vcf/AbstractVCFCodecTest.java
@@ -0,0 +1,22 @@
+package htsjdk.variant.vcf;
+
+import htsjdk.variant.VariantBaseTest;
+import htsjdk.variant.variantcontext.VariantContext;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+
+public class AbstractVCFCodecTest extends VariantBaseTest {
+    @Test
+    public void shouldPreserveSymbolicAlleleCase() {
+        VCFFileReader reader = new VCFFileReader(new File(VariantBaseTest.variantTestDataRoot + "breakpoint.vcf"), false);
+        VariantContext variant = reader.iterator().next();
+        reader.close();
+
+        // VCF v4.1 s1.4.5
+        // Tools processing VCF files are not required to preserve case in the allele String, except for IDs, which are case sensitive.
+        Assert.assertTrue(variant.getAlternateAllele(0).getDisplayString().contains("chr12"));
+    }
+}
